27025d81f31a007bcf95085e7eaae042947c062b
Bobby Johnson <bobby@madnesslabs.net>
2021-01-27 18:56:08 -0600

    fix(): Change the way interfaces are exported

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -1,52 +1,40 @@
-export declare interface OrganismComponent {
+export interface OrganismComponent {
+  docs: string;
+  docsTags: any[];
+  encapsulation: string;
+  events: any[];
+  methods: any[];
+  presets?: {
+    [presetName: string]: OrganismComponent;
+  };
+  props: {
+    attr: string;
+    default: any;
     docs: string;
     docsTags: any[];
-    encapsulation: string;
-    events: any[];
-    methods: any[];
-    presets?: {
-      [presetName: string]: OrganismComponent
-    };
-    props: {
-      attr: string;
-      default: any;
-      docs: string;
-      docsTags: any[];
-      mutable: boolean;
-      name: string;
-      optional: boolean;
-      reflectToAttr: boolean;
-      required: boolean;
-      type: string;
-    }[];
-    readme: string;
-    slots: any[];
-    styles: any[];
-    tag: string;
-    usage: any;
+    mutable: boolean;
+    name: string;
+    optional: boolean;
+    reflectToAttr: boolean;
+    required: boolean;
+    type: string;
+  }[];
+  readme: string;
+  slots: any[];
+  styles: any[];
+  tag: string;
+  usage: any;
+}
+
+export interface OrganismPreset {
+  name?: string;
+  description?: string;
+  props?: any;
+  innerHTML?: (component: any, props: any) => string;
+  beforeHTML?: (component: any, props: any) => string;
+  afterHTML?: (component: any, props: any) => string;
+  hooks?: {
+    onSetPreset: (data?: { organismEl?: HTMLFireenjinDesignerOrganismElement; props?: any; preset?: OrganismPreset; presetName?: string; component?: OrganismComponent }) => void;
+    onUpdateProp?: (data?: { organismEl?: HTMLFireenjinDesignerOrganismElement; props?: any; preset?: OrganismPreset; propName?: string; component?: OrganismComponent }) => void;
   };
-  
-export declare interface OrganismPreset {
-    name?: string,
-    description?: string;
-    props?: any;
-    innerHTML?: (component: any, props: any) => string, 
-    beforeHTML?: (component: any, props: any) => string, 
-    afterHTML?: (component: any, props: any) => string,
-    hooks?: {
-      onSetPreset: (data?: {
-        organismEl?: HTMLFireenjinDesignerOrganismElement;
-        props?: any;
-        preset?: OrganismPreset;
-        presetName?: string;
-        component?: OrganismComponent;
-      }) => void;
-      onUpdateProp?: (data?: {
-        organismEl?: HTMLFireenjinDesignerOrganismElement;
-        props?: any;
-        preset?: OrganismPreset;
-        propName?: string;
-        component?: OrganismComponent;
-      }) => void;
-    }
-  }
+}
